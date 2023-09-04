74360e63911e90740d0145b2ce6afa5d5db58b1c
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-09-05 00:43:33 +0200

    Add ShaderReplaceDemosaic

---
+++ ShaderReplaceDemosaic/ShaderReplaceDemosaicPlugin.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using DemosaicCommon;
+using UnityEngine;
+
+namespace ShaderReplaceDemosaic
+{
+    /// <summary>
+    /// Smarter version of DumbRendererDemosaic, uncensors some Live2D games where privates completely disappear with other demosaic
+    /// </summary>
+    [BepInPlugin("manlymarco.ShaderReplaceDemosaic", "Shader Replace Demosaic", Metadata.Version)]
+    public class ShaderReplaceDemosaicPlugin : BaseUnityPlugin
+    {
+        private Shader _goodShader;
+
+        private ConfigEntry<string> _nameSetting;
+        private ConfigEntry<string> _targetSetting;
+
+        private void Awake()
+        {
+            _nameSetting = Config.Bind("Shader replace", "Replacement shader name", "Body", "Part or whole name of the shader that should be used to replace mozaic shaders. Case sensitive.");
+            _nameSetting.SettingChanged += (sender, args) => _goodShader = null;
+
+            _targetSetting = Config.Bind("Shader replace", "Target shader name", "", "Part or whole name of the shader (or material it is on) to be replaced. Case sensitive. If empty, common names of mozaic shaders/materials are searched.");
+        }
+
+        private void Update()
+        {
+            foreach (var renderer in FindObjectsOfType<SkinnedMeshRenderer>())
+            {
+                if (renderer == null)
+                    continue;
+
+                foreach (var material in renderer.sharedMaterials.Length == 0 ? renderer.materials : renderer.sharedMaterials)
+                {
+                    if (material == null)
+                        continue;
+
+                    if (_targetSetting.Value.Length == 0 ? (MozaicTools.IsMozaicName(material.name) || MozaicTools.IsMozaicName(material.shader.name)) : (material.name.Contains(_targetSetting.Value) || material.shader.name.Contains(_targetSetting.Value)))
+                    {
+                        if (_goodShader == null) continue;
+
+                        if (material.shader != _goodShader)
+                        {
+                            Logger.LogInfo($"Replacing shader {material.shader.name} on material {material.name} on renderer {MozaicTools.GetTransformPath(renderer.transform)} with shader {_goodShader.name}");
+                            material.shader = _goodShader;
+                        }
+                    }
+                    else if (_goodShader == null && material.shader.name.Contains(_nameSetting.Value))
+                    {
+                        Logger.LogInfo($"Found replacement shader {material.shader.name} on material {material.name} on renderer {MozaicTools.GetTransformPath(renderer.transform)}");
+                        _goodShader = material.shader;
+                    }
+                }
+            }
+        }
+    }
+}
